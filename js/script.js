31e4bf08d6e2fea9e3b08ea0cfdf342e67a67382
NeyleCM <neylecmorantes@gmail.com>
2024-06-26 00:43:28 +0200

    JS listo, aún falta CSS

---
+++ js/script.js
@@ -0,0 +1,45 @@
+//  Cada vez que se recarga la página debe añadir una visita más
+// Debe persistir el número aunque se cierre el navegador
+// i deseas reiniciar el contador, haz clic en el botón 
+// "Reestablecer Contador" y se pondrá a cero y comenzará 
+// de nuevo en 1 en la siguiente visita.
+
+const contadorCero = document.getElementById('btnReestablecer');
+const contadorVisitas = document.getElementById('contadorVisitas')
+
+contadorCero.addEventListener( 'click', () => {
+    localStorage.setItem('visitas', 0);
+    contadorVisitas.textContent = 0 ;
+})
+/*
+let visitas = parseFloat(localStorage.getItem('visitas')) || 0;
+
+visitas += 1;
+
+localStorage.setItem('visitas', visitas);
+contadorVisitas.textContent = visitas;
+*/
+let visitaWeb;
+
+if(localStorage.visitas) {
+    visitaWeb = parseFloat(localStorage.visitas)+1;
+}
+localStorage.visitas = visitaWeb
+contadorVisitas.textContent = visitaWeb;
+
+/*
+if(localStorage.getItem('visitas')) {
+    localStorage.getItem('visitas')+1;
+} 
+    
+else{localStorage.setItem('visitas', 1) 
+
+}
+*/
+//contadorVisitas.textContent = visitaCorrecto;
+
+
+//console.log(contador)
+//localStorage.setItem('visitas', ' ');
+
+//const contador = 
